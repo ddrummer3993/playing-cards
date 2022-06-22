a75839fd02fad4cf800ea4d31f8223d56c82eb39
ddrummer <drumkdav@amazon.com>
2022-06-22 18:28:30 -0400

    Made comparator a lambda in tally.

--- src/main/java/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/CardTrick.java
@@ -67,34 +67,16 @@ public class CardTrick {
             }
         }
 
-        //below: created local class in tally method.
-        class DisplayComparator implements Comparator<Card> {
 
-            @Override
-            public int compare(Card card1, Card card2) {
+        Comparator<Card> comparator = (card1, card2) -> {
                 int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
                 comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
                 comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
                 return comparison;
-            }
-
-        }
-
-        // below: defines anonymous class, calls instance, and assigns it to "comparator" field.
-        Comparator<Card> comparator = new Comparator<>() {
-
-            @Override
-            public int compare(Card card1, Card card2) {
-                int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor());
-                comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
-                comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
-                return comparison;
-            }
-
         };
 
-        Collections.sort((LinkedList<Card>) blackPile, comparator);
-        Collections.sort((LinkedList<Card>) redPile, comparator);
+        ((LinkedList<Card>) blackPile).sort(comparator);
+        ((LinkedList<Card>) redPile).sort(comparator);
 
         System.out.printf("Black: count=%d, cards=%s%n", blackCount, blackPile);
         System.out.printf("Red: count=%d, cards=%s%n", redCount, redPile);
